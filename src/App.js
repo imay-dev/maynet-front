8ed6d4322bc48a1b5ec2a6c3c0961d4d07982d55
Maysam Rabipour <maysam.rabipour@gmail.com>
2019-11-08 21:09:00 +0330

    add register/login actions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import { Provider } from 'react-redux'
 import store from './store'
+
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser, logoutUser } from './actions/authActions'
+
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -12,6 +16,32 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+
+
+// Check for Token
+if(localStorage.jwtToken) {
+  // Set Auth Token Header
+  setAuthToken(localStorage.jwtToken)
+
+  // Decode Token and Get User Info and Exp
+  const decoded = jwt_decode(localStorage.jwtToken)
+  // Set User and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+
+
+  // Check for Expired Token
+  const currentTime = Date.now() / 1000
+  
+  if(decoded.exp < currentTime) {
+    // Logout User
+    store.dispatch(logoutUser())
+    
+    // TODO: Clear Current Profile
+
+    // Redirect to Login
+    window.location.href = '/login'
+  }
+}
 
 
 function App() {
